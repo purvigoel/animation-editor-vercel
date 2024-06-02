a84dfd672974ee167121583fab01935cd8bfdcd8
ststrong <seantstrong@gmail.com>
2024-06-02 09:18:31 -0700

    Adding frame editor field

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [currentFrame, setCurrentFrame] = useState(0);
+  const [totalFrames, setTotalFrames] = useState(60);
+
 
   useEffect(() => {
     fetch('/data/file1.npy')
@@ -18,33 +22,65 @@ export default function Home() {
       });
   }, []);
 
+  const handleDownload = () => {
+    console.log("Downloading...");
+  }
+
+  const handlePositions = () => {
+    console.log("Downloading...");
+  }
+
+
+
   return (
     <main className="h-full w-full h-screen">
-      <div className="flex flex-col w-full h-full bg-red-500 text-white">
+      <div className="flex flex-col w-full h-full">
         {/* Top bar */}
-        <div className="flex  w-full justify-between  opacity-50 p-3">
-          <button className="bg-white text-red-500 p-1 rounded">Button 1</button>
-          <button className="bg-white text-red-500 p-1 rounded">Button 2</button>
-          <button className="bg-white text-red-500 p-1 rounded">Button 3</button>
+        <div className="flex  w-full bg-gray-100 justify-between p-3">
+          <button className="bg-red-200 text-red-500 font-semibold text-sm p-2 rounded mx-2">
+            Joint Positions
+          </button>
+          <form className="flex">
+            <input
+              type="number"
+              name="frameInput"
+              className="bg-white text-black p-2 rounded w-16 "
+              value={currentFrame == 0 ? "" : currentFrame}
+              onBlur={(e) => {
+                const frame = parseInt((e.target as HTMLInputElement).value, 10);
+                if (isNaN(frame)) {
+                  (e.target as HTMLInputElement).value = "0";
+                }
+              }}
+              onChange={(e) => {
+                const frame = parseInt((e.target as HTMLInputElement).value, 10);
+                if (!isNaN(frame)) {
+                  const frame_constrained = frame > totalFrames ? totalFrames : frame < 0 ? 0 : frame;
+                  setCurrentFrame(frame_constrained);
+                } else {
+                  setCurrentFrame(0);
+                }
+              }}
+            />
+            <span className="p-2 rounded mx-2">
+              / {totalFrames}
+            </span>
+          </form>
+          <button className="bg-red-200 text-red-500 font-semibold text-sm p-2 rounded mx-2">
+            Download
+          </button>
         </div>
         {/* Canvas */}
-        <div className="flex-grow flex flex-col w-full h-full bg-blue-500 text-white">
-          <canvas id="mainCanvas" className="border border-green-500 bg-purple w-full h-full"></canvas>
+        <div className="flex-grow flex flex-col w-full h-full bg-white text-white">
+          <canvas id="mainCanvas" className="border border-black w-full h-full"></canvas>
         </div>
         {/* Bottom bar */}
-        <div className="flex flex-col w-full bg-gray-300 text-white p-3">
-          <input type="range" className="w-full bg-white" />
+        <div className="flex flex-col w-full bg-gray-200 text-white p-3">
+          <input type="range" className="w-full bg-white" min={0} max={totalFrames} value={currentFrame} onChange={(e) => setCurrentFrame(parseInt(e.target.value, 10) || 0)} />
         </div>
 
 
       </div>
-      {/* <div className="w-full bg-gray-400 p-2 text-white flex justify-between fixed top-0">
-
-      </div>
-      <div className="flex-grow flex flex-col items-center justify-center w-full h-full">
-        
-      </div>
-  */}
     </main>
   );
 }
